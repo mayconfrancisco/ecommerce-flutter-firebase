9bcd9f5ac8441226c4df46b55ee5b0688bd0c824
Maycon <mayconjf@gmail.com>
2020-10-16 13:36:39 -0300

    adicionada lista de produtos

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -3,14 +3,79 @@ import 'package:flutter/material.dart';
 
 class ProductTile extends StatelessWidget {
   final String type;
-  final ProductData data;
+  final ProductData product;
 
-  ProductTile(this.type, this.data);
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(data.title),
+    return InkWell(
+      onTap: () {},
+      child: Card(
+          child: "grid" == this.type
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 0.8,
+                      child: Image.network(
+                        product.images[0],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Expanded(
+                        child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            ' R\$ ${product.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                                color: Theme.of(context).primaryColor),
+                          )
+                        ],
+                      ),
+                    ))
+                  ],
+                )
+              : Row(
+                  children: [
+                    Flexible(
+                        flex: 1,
+                        child: Image.network(
+                          product.images[0],
+                          // height: 200,
+                        )),
+                    Flexible(
+                        flex: 1,
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                product.title,
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              ),
+                              Text(
+                                'R\$ ${product.price.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17,
+                                    color: Theme.of(context).primaryColor),
+                              )
+                            ],
+                          ),
+                        )),
+                  ],
+                )),
     );
   }
 }
